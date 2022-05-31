5f731bf5ce50f025c96671cb6c250a48ee1c2944
Letícia Santos <104040490+1Leticia24@users.noreply.github.com>
2022-05-31 01:03:54 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,12 @@
+const nome = "Leticia Santos";
+const idade = 19;
+const altura = 1.65;
+const comida = brigadeiro;
+let verdadeiro = true;
+let falso = false;
+
+console.log(
+  `Meu nome é ${nome}, eu tenho ${idade} anos, minha altura é ${altura} minha comida favorita é ${comida}`
+);
+
+console.log( `Amante da Arte e da Música: ${verdadeiro}`)
