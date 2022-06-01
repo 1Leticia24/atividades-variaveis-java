43ff2eb1b1c55954278ba2c43764ca3fd3adaa85
Letícia Santos <104040490+1Leticia24@users.noreply.github.com>
2022-06-01 13:57:42 -0300

    Add files via upload

---
+++ variaveis2.js
@@ -0,0 +1,14 @@
+//Crie um algoritmo que calcule a velocidade média de um veículo e retorne o resultado no console.
+//cria uma variavel para guardar o valor da distancia percorrida.
+//cria uma variavel para guardar o valor do tempo gasto
+//cria uma variavel para guardar o resutado da divisao entre a distacia percorrida e o tempo gasto, our seja, a velocidade media
+//comando para aparecer no console em uma string interpolation.
+
+
+let distanciaPercorrida = 150;
+let tempoGasto = 5;
+let velocidadeMedia = distanciaPercorrida/tempoGasto;
+
+console.log(`A velocidade média que fiz o trajeto é de ${velocidadeMedia} km/h.`)
+
+//* " A velocidade média que fiz o trajeto é de 30km/h." 
